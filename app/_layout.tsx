6e2600a4818385410998a4b6ec5a64b136e6a392
unknown <miguelmbastos75@gmail.com>
2025-10-04 10:04:24 -0300

    fix: remove uso remanescente do VoicePageAnnouncer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider, useTheme } from '../components/ThemeContext';
 import { VoiceCommandProvider } from '../components/VoiceCommandContext'; // Importe o novo provider
 import { useEffect } from 'react';
 import * as NavigationBar from 'expo-navigation-bar';
-import { VoicePageAnnouncer } from 'components/VoicePageAnnouncer';
 
 // Componente interno para acessar o contexto do tema
 function ThemedSystemBars() {
@@ -23,12 +22,10 @@ function ThemedSystemBars() {
 
 export default function RootLayout() {
   return (
-    // Envolva com ambos os providers. ThemeProvider primeiro.
     <ThemeProvider>
       <VoiceCommandProvider>
         <SafeAreaProvider>
           <ThemedSystemBars />
-          <VoicePageAnnouncer />
           <Stack screenOptions={{ headerShown: false }} />
         </SafeAreaProvider>
       </VoiceCommandProvider>
